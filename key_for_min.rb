04b6171791acb1275243ba5d30e15c5fc228b87e
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-28 01:48:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,6 @@ def key_for_min_value(name_hash)
   lower_value = nil
 lower_key = nil
 name_hash.each do |key,value|
-  
+  if smallest_value == nil && value < smallest_value
+    smallest_value = key
 end
